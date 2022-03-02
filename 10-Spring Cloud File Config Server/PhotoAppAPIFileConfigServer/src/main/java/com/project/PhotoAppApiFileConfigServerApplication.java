374fe31047c1bc6d38cbae54263fe0e27fbc37fa
Taha Topal <taha.topal.7@gmail.com>
2022-03-02 21:32:07 +0300

    before docker

---
+++ 10-Spring Cloud File Config Server/PhotoAppAPIFileConfigServer/src/main/java/com/project/PhotoAppApiFileConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class PhotoAppApiFileConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoAppApiFileConfigServerApplication.class, args);
+    }
+
+}
---
+++ 11-Albums Microservice/AlbumAppApiUsers/src/test/java/com/example/albumappapiusers/AlbumAppApiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.albumappapiusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumAppApiUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
